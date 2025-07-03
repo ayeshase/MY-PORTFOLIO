64cd7bc322f918fa3175d7faf84d3633f0bcb2da
unknown <ayeshazayn313@gmail.com>
2025-07-03 15:37:38 +0500

    add some styles and some changes into about page

---
+++ about.js
@@ -0,0 +1,26 @@
+  <div class="section">
+        <div class="container">
+            <div class="content-section">
+                <div class="title">
+                    <h1>About MySelf</h1>
+                </div>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod dolore pariatur,
+                        nemo id quisquam nobis minus modi illo in distinctio. Explicabo, asperiores?
+                        Earum odit fuga voluptates quae dicta molestiae saepe.</p>
+                </div>
+                <div class="buttons">
+
+                    <button class="btn-left">2 years <br />of experiance</button>
+                    <button class="btn-right"> 5 + Active <br />Projects</button>
+
+                </div>
+
+                
+
+            </div class="image-section">
+            <div><img src="dev.png">
+            </div>
+
+        </div>
+    </div>
